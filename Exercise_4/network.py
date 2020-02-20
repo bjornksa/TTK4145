598481fc01f86cc3b051b1a4239445dd113f6f8a
Bjørn Kåre Sæbø <bjornksa@stud.ntnu.no>
2020-02-20 12:34:50 +0100

    Network_update

--- Exercise_4/network.py
+++ Exercise_4/network.py
@@ -5,20 +5,26 @@ from time import sleep
 
 SENDER_IP = socket.gethostname()
 BROADCAST_PORT = 1440
+PRIVATE_PORT = 1560
 
-def send(message, ip):
-    pass
+
+def send(type, response, ip):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    message = {
+        "messagetype": type,
+        "sender_ip": SENDER_IP,
+        "response": response
+    }
+    s.sendto(json.dumps(message), (ip, PRIVATE_PORT))
+    s.close()
 
 
 def broadcast(type, order):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    message = {
-        "messagetype": type,
-        "order" : order
-    }
+    message = {"messagetype": type,"sender_ip": SENDER_IP, "order": order    }
     message_string = json.dumps(message)
-    s.sendto(message_string.encode(), 1024, ("255.255.255.255", BROADCAST_PORT))
+    s.sendto(message_string.encode(), ("255.255.255.255", BROADCAST_PORT))
     s.close()
 
 
