18fd8cba563bab0785c7c100d99bdd5aee022b81
simenhu <simenhusoy@gmail.com>
2020-02-20 14:15:18 +0100

    halv testeting

---
+++ wrapper_tester/c_tester.py
@@ -0,0 +1,24 @@
+import ctypes
+import threading
+import time
+
+testlib = ctypes.CDLL('/home/simen/Documents/school/sanntidsprogrammering/wrapper_tester/test.so')
+
+def notification_tester(name, t):
+    while True:
+        testlib.simple_timer(t)
+        print(f'Timer {name} firing!!!')
+
+def main_tester():
+    testlib.main()
+
+if __name__ == '__main__':
+    x = threading.Thread(target=notification_tester, args=('simens tråd', 1))
+    y = threading.Thread(target=notification_tester, args=('Erlends tråd', 4))
+    main_thread = threading.Thread(target=main_tester)
+    main_thread.start()
+    x.start()
+    y.start()
+    main_thread.join()
+    x.join()
+    y.join()
