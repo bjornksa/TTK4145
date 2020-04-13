63cdc65f41780077e054df4c6932083d489e95c2
Erlend Blomseth <erlend@elendig.no>
2020-04-13 19:05:06 +0200

    broadcast funker, kokt fra exercise4

---
+++ src/network.py
@@ -0,0 +1,41 @@
+import json  #parse = loads(), serialize = dumps()
+import socket
+
+from time import sleep
+
+SENDER_IP = socket.gethostname()
+BROADCAST_PORT = 1440
+PRIVATE_PORT = 1560
+
+def send(type, data, ip):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    message = {
+        "type": type,
+        "sender_ip": SENDER_IP,
+        "data": data
+    }
+    s.sendto(json.dumps(message), (ip, PRIVATE_PORT))
+    s.close()
+
+def broadcast(type, data):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    message = {
+        "type": type,
+        "sender_ip": SENDER_IP,
+        "data": data
+    }
+    message_string = json.dumps(message)
+    s.sendto(message_string.encode(), ("255.255.255.255", BROADCAST_PORT))
+    s.close()
+
+if __name__ == "__main__":
+    order = {
+        "ip": "192.168",
+        "floor": 2,
+        "button": 1
+    }
+    while True:
+        broadcast("cost_request", order)
+        print("sent", json.dumps(order))
+        sleep(1)
